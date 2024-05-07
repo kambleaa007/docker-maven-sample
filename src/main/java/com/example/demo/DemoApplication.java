fabd3e2b29a75843d5494d32f03bd4dbd342a82c
Ashish Kamble <ashishkamble191191@gmail.com>
2024-05-07 16:54:56 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.printf("Hello and welcome!");
+	
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println("i = " + i);
+	        }
+		
 		SpringApplication.run(DemoApplication.class, args);
+		
+
 	}
 
 }
